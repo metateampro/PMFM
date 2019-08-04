0f7f519d1bd8570a03e52548508504ad1427e819
Yegor <mazda22x@mail.ru>
2019-08-04 16:23:18 +0300

    Autrhorization + MainPage structure

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,40 @@
 <template>
   <v-app>
+    <v-navigation-drawer app>
+      <!-- -->
+    </v-navigation-drawer>
+
     <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
+      <!-- -->
     </v-app-bar>
 
+    <!-- Sizes your content based upon application components -->
     <v-content>
-      <HelloWorld/>
+
+      <!-- Provides the application the proper gutter -->
+      <v-container fluid>
+
+        <!-- If using vue-router -->
+        <router-view></router-view>
+      </v-container>
     </v-content>
+
+    <v-footer app>
+      <!-- -->
+    </v-footer>
   </v-app>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import HelloWorld from './components/HelloWorld.vue';
+import Authorization from './components/Authorization.vue';
 
 export default Vue.extend({
   name: 'App',
   components: {
     HelloWorld,
+    Authorization,
   },
   data: () => ({
     //
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
+
+
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import './registerServiceWorker';
 import vuetify from './plugins/vuetify';
+
 
 Vue.config.productionTip = false;
 
